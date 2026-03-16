621caa64da07ad9bd0d763c0ff42503e5a182a1d
axelup5 <axelup5@gmail.com>
2026-03-16 16:28:07 -0300

    Add ExamplePlayerCommand

---
+++ src/main/java/dev/hytalemodding/commands/ExamplePlayerCommand.java
@@ -0,0 +1,34 @@
+package dev.hytalemodding.commands;
+
+import javax.annotation.Nonnull;
+
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.entity.UUIDComponent;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+
+
+public class ExamplePlayerCommand extends AbstractPlayerCommand {
+
+    public ExamplePlayerCommand () {
+        super("playertest", "Super Test Command!");
+    }
+
+    @Override
+    protected void execute(@Nonnull CommandContext context, @Nonnull Store<EntityStore> store, @Nonnull Ref<EntityStore> ref, @Nonnull PlayerRef playerRef, @Nonnull World world) {
+        Player player = store.getComponent(ref, Player.getComponentType());
+        UUIDComponent component = store.getComponent(ref, UUIDComponent.getComponentType());
+        TransformComponent transform = store.getComponent(ref, TransformComponent.getComponentType());
+        player.sendMessage(Message.raw("Player#getUuid() : " + player.getUuid()));
+        player.sendMessage(Message.raw("UUIDComponent : " + component.getUuid()));
+        player.sendMessage(Message.raw("Equal : " + player.getUuid().equals(component.getUuid())));
+        player.sendMessage(Message.raw("Transform : " + transform.getPosition()));
+    }
+}
