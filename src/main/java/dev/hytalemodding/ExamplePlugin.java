096c4d81a010e6fab7775526e19f03ddadd6c6d9
axelup5 <axelup5@gmail.com>
2026-03-16 21:17:12 -0300

    Add ExampleTargetEntityCommand and HealPlayerCommand to ExamplePlugin

--- src/main/java/dev/hytalemodding/ExamplePlugin.java
+++ src/main/java/dev/hytalemodding/ExamplePlugin.java
@@ -5,6 +5,8 @@ import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import dev.hytalemodding.commands.ExampleCommand;
 import dev.hytalemodding.commands.ExamplePlayerCommand;
+import dev.hytalemodding.commands.ExampleTargetEntityCommand;
+import dev.hytalemodding.commands.HealPlayerCommand;
 import dev.hytalemodding.commands.ServerRulesCommand;
 import dev.hytalemodding.events.ExampleEvent;
 
@@ -21,6 +23,8 @@ public class ExamplePlugin extends JavaPlugin {
         this.getCommandRegistry().registerCommand(new ExampleCommand("brutalimpacts", "An example command from de Brutal Impacts Mod."));
         this.getCommandRegistry().registerCommand(new ExamplePlayerCommand());
         this.getCommandRegistry().registerCommand(new ServerRulesCommand());
+        this.getCommandRegistry().registerCommand(new ExampleTargetEntityCommand());
+        this.getCommandRegistry().registerCommand(new HealPlayerCommand());
         this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, ExampleEvent::onPlayerReady);
     }
 }
