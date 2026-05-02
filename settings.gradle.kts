2c3c15e3cb7490d492de4aed7aa9819b12ba892c
axelup5 <axelup5@gmail.com>
2026-05-02 16:05:39 -0300

    feat: Update Brutal Impacts to version 2.3.3 with UI improvements and bug fixes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
     }
 }
 
-rootProject.name = "Brutal Impacts (pre-release) v2.3.2"
+rootProject.name = "Brutal Impacts (pre-release) v2.3.3"
 
 plugins {
     // See documentation on https://scaffoldit.dev
